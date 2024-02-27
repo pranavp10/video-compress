94e5826403ceea7aa8d5c095d16bcfccd03360cf
Pranav <pranavkp.me@outlook.com>
2024-02-27 16:27:09 +0530

    added button on hero

--- app/(landing)/components/hero.tsx
+++ app/(landing)/components/hero.tsx
@@ -23,6 +23,21 @@ const Hero = () => (
       even offline. And the best part?{" "}
       <span className="text-black font-medium">It&apos;s free!</span>
     </h2>
+    <div className="flex gap-4 items-center justify-center mt-10">
+      <a
+        href="/video"
+        className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white/90 px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 flex-shrink-0"
+      >
+        Compress Now
+      </a>
+      <a
+        href="https://github.com/pranavp10/video-compress"
+        target="_blank"
+        className="border rounded-lg  px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 flex-shrink-0 flex items-center gap-2"
+      >
+        Star on Github
+      </a>
+    </div>
   </div>
 );
 
--- app/(landing)/components/videoDemo.tsx
+++ app/(landing)/components/videoDemo.tsx
@@ -1,18 +1,16 @@
-const VideoDemo = () => {
-  return (
-    <div className="px-6 relative">
-      <video
-        autoPlay
-        loop
-        playsInline
-        preload="auto"
-        className="h-full rounded-3xl w-full object-cover border-8 overflow-clip"
-      >
-        <source src="/demo.mp4" type="video/mp4" />
-        {/* <source src="/img/landing/video-software.webm" type="video/webm" /> */}
-      </video>
-    </div>
-  );
-};
+const VideoDemo = () => (
+  <div className="px-6 relative">
+    <video
+      autoPlay
+      loop
+      playsInline
+      preload="auto"
+      className="h-full rounded-3xl w-full object-cover border-8 overflow-clip"
+    >
+      <source src="/demo.mp4" type="video/mp4" />
+      {/* <source src="/img/landing/video-software.webm" type="video/webm" /> */}
+    </video>
+  </div>
+);
 
 export default VideoDemo;
