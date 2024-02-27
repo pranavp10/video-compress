abf9b47f748979f2d06e72e3f08ce0b42a81df44
Pranav <pranavkp.me@outlook.com>
2024-02-27 18:33:23 +0530

    about page added

---
+++ app/about/page.tsx
@@ -0,0 +1,62 @@
+const Page = () => {
+  return (
+    <div className="max-w-5xl mx-auto pt-36 space-y-10 px-6 sm:px-0 pb-10">
+      <h1 className="text-4xl font-bold  lg lg:font-semibold text-gray-900">
+        The Story Behind Compress
+      </h1>
+      <div className="sm:text-lg space-y-5 text-gray-500 font-medium">
+        <p>
+          It all started with a{" "}
+          <span className="text-black">burning need.</span> As a developer, I
+          was constantly juggling a hodgepodge of tools to manipulate media
+          files. Each one offered a single feature - compression, trimming,
+          conversion - but none offered the{" "}
+          <span className="text-black">
+            comprehensive and user-friendly experience
+          </span>{" "}
+          I craved.
+        </p>
+        <p>
+          Sure, the command line was an option, but what about the non-technical
+          folks out there? Weren&apos;t they deserving of a{" "}
+          <span className="text-black">simple and intuitive solution too?</span>
+        </p>
+        <p>
+          Desperate for a better way, I embarked on a quest. I scoured the web,
+          hoping to find an existing tool that ticked all the boxes. But alas, I
+          was met with a disheartening reality. Many tools boasted powerful
+          features, but their{" "}
+          <span className="text-black">cluttered interfaces</span> were enough
+          to send shivers down my spine.
+        </p>
+        <p>
+          Then, a spark ignited within me. Why not{" "}
+          <span className="text-black">bridge the gap</span> myself? I could
+          leverage my coding skills to create a tool that combined
+          <span className="text-black"> powerful functionality</span> with a{" "}
+          <span className="text-black">user-friendly design.</span> ✨
+        </p>
+        <p>
+          And so, <span className="text-black">Compress</span> was born. It
+          embodies the belief that
+          <span className="text-black">
+            powerful tools shouldn&apos;t be shrouded in complexity
+          </span>
+          . It&apos;s a testament to the fact that{" "}
+          <span className="text-black">
+            {" "}
+            technology can be both effective and accessible.
+          </span>
+        </p>
+        <p>
+          This isn&apos;t just a media manipulation tool; it&apos;s a{" "}
+          <span className="text-black">passion project</span> fueled by a desire
+          to make life easier for everyone. Join the journey and experience the
+          difference <span className="text-black">Compress</span> can make!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
