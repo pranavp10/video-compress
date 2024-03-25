33d3aaf63d9dc36a3c5abbcb945fd5a4c317d039
Pranav <pranavkp.me@outlook.com>
2024-03-25 20:20:11 +0530

    updated the script for analytics

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,13 +1,18 @@
+import Script from "next/script";
+
 const Page = () => {
   return (
     <>
       <iframe
-        plausible-embed=""
         src="https://plausible.prolab.sh/videocompress.prolab.sh?embed=true&theme=light&background=transparent"
         scrolling="no"
         frameBorder={0}
         loading="lazy"
         style={{ width: 1, minWidth: "100%", height: 1600 }}
+      />
+      <Script
+        async
+        src="https://plausible.prolab.sh/videocompress.prolab.sh/js/embed.host.js"
       />
     </>
   );
