dff6351d7a2f13ae752dea397153bc9c2b8d50f5
Pranav <pranavkp.me@outlook.com>
2024-04-10 19:46:38 +0530

    add glitchtip to the project to log errors

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "http://867608a9d67d45f981364e81ee66aed8@glitchtip.prolab.sh/1",
+  tracesSampleRate: 1,
+  debug: false,
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  integrations: [
+    Sentry.replayIntegration({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
