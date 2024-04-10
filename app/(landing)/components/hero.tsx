ceae5505f96b059cfa578c2b73f29ec3be464d65
Pranav <pranavkp.me@outlook.com>
2024-04-10 19:48:46 +0530

    updated the Hero page

--- app/(landing)/components/hero.tsx
+++ app/(landing)/components/hero.tsx
@@ -1,3 +1,4 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import React from "react";
 
@@ -36,7 +37,7 @@ const Hero = () => (
         target="_blank"
         className="border rounded-lg  px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 flex-shrink-0 flex items-center gap-2"
       >
-        Star on Github
+        <GitHubLogoIcon /> Github
       </a>
     </div>
   </div>
